e45c932687f8f62513edaaa2d244c91e1fe9dfdd
Dominionss <qshieldt@gmail.com>
2025-03-24 12:04:51 +0200

    Notes and Category models.

--- notes_app/models.py
+++ notes_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    reminder = models.BooleanField(default=False)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='notes')
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
