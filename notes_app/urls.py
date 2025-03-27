46744ea9105d4a6b00a600e3643de4a4a2279331
Dominionss <qshieldt@gmail.com>
2025-03-27 23:13:13 +0200

    Begin of creating functional buttons.

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import home
+from .views import home, create_note
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('', home, name='home'),
+    path('create/', create_note, name='create_note'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- notes_app/views.py
+++ notes_app/views.py
@@ -5,3 +5,6 @@ from .models import Note
 def home(request):
     notes = Note.objects.all()
     return render(request, 'home.html', {'notes': notes})
+
+def create_note(request):
+    return render(request, 'create.html')
